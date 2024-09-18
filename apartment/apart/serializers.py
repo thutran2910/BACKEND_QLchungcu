445800597c3da2b7c926f8572f76e5c640ae543e
Trinh Doan Hau <trinhdoanhauu@gmail.com>
2024-09-18 18:17:10 +0700

    update BE dang nhap, dang ky

--- apartment/apart/serializers.py
+++ apartment/apart/serializers.py
@@ -5,6 +5,47 @@ from rest_framework.serializers import ModelSerializer
 from .models import Resident, Flat, Bill, Item, Feedback, Survey, FaMember, SurveyResult, Product, Cart, \
     CartProduct, OrderProduct, Order
 
+
+
+class ResidentSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField()
+    avatar = serializers.ImageField(write_only=True, required=False)
+    is_staff = serializers.BooleanField(required=False, default=False)
+    is_superuser = serializers.BooleanField(required=False, default=False)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    def get_avatar_url(self, instance):
+        if instance.avatar:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(instance.avatar.url)
+            return instance.avatar.url
+        return None
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['avatar_url'] = self.get_avatar_url(instance)
+        return rep
+
+    def create(self, validated_data):
+        avatar = validated_data.pop('avatar', None)
+        resident = Resident(**validated_data)
+        resident.set_password(validated_data['password'])
+        if avatar:
+            resident.avatar = avatar
+        resident.save()
+        return resident
+
+    class Meta:
+        model = Resident
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'username', 'password', 'avatar', 'avatar_url',
+                  'is_staff', 'is_superuser']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_active': {'read_only': True}
+        }
+
+
 class BillSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source='resident.first_name', read_only=True)
     last_name = serializers.CharField(source='resident.last_name', read_only=True)
