294e840b1ed1aa85cf425d546bf19abcc2102ce8
thutran2910 <111982491+thutran2910@users.noreply.github.com>
2024-09-18 17:15:10 +0700

    update BE Hoadon

---
+++ apartment/apart/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import ModelSerializer
+from .models import Resident, Flat, Bill, Item, Feedback, Survey, FaMember, SurveyResult, Product, Cart, \
+    CartProduct, OrderProduct, Order
+
+class BillSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source='resident.first_name', read_only=True)
+    last_name = serializers.CharField(source='resident.last_name', read_only=True)
+    phone = serializers.CharField(source='resident.phone', read_only=True)
+    resident_id = serializers.PrimaryKeyRelatedField(queryset=Resident.objects.all(), write_only=True, source='resident')
+    image_url = serializers.SerializerMethodField()
+    image = serializers.ImageField(write_only=True, required=False)
+    avatar_url = serializers.SerializerMethodField()
+
+    def get_image_url(self, instance):
+        if instance.image:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(instance.image.url)
+            return instance.image.url
+        return None
+    def get_avatar_url(self, instance):
+        if instance.resident.avatar:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(instance.resident.avatar.url)
+            return instance.resident.avatar.url
+        return None
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['image_url'] = self.get_image_url(instance)
+        rep['avatar_url'] = self.get_avatar_url(instance)
+        return rep
+    class Meta:
+        model = Bill
+        fields = '__all__'
--- apartment/apart/views.py
+++ apartment/apart/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+import hashlib
+import hmac
+from datetime import time
+import requests
+from django.db.models import Max
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, JsonResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets, permissions, status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from . import paginators
+from .models import Flat, Item, Resident, Feedback, Survey, SurveyResult, Bill, FaMember, Cart, Product, CartProduct, \
+    Order, OrderProduct
+from .serializers import ResidentSerializer, FlatSerializer, ItemSerializer, FeedbackSerializer, SurveySerializer, \
+    SurveyResultSerializer, BillSerializer, FaMemberSerializer, CartSerializer, ProductSerializer, \
+    CartProductSerializer, OrderSerializer
 
-# Create your views here.
+
+
+class BillViewSet(viewsets.ModelViewSet):
+    serializer_class = BillSerializer
+    #pagination_class = paginators.Paginator
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        resident = self.request.user
+        if resident.is_superuser:
+            queryset = Bill.objects.all()
+        else:
+            queryset = Bill.objects.filter(resident=resident)
+
+        payment_status = self.request.query_params.get('payment_status', None)
+        if payment_status:
+            queryset = queryset.filter(payment_status=payment_status.upper())
+        return queryset
+
+    @action(methods=['get'], detail=False, url_path='total-bills')
+    def total_bills(self, request, *args, **kwargs):
+        resident = self.request.user
+        if resident.is_superuser:
+            bills = Bill.objects.all()
+        else:
+            bills = Bill.objects.filter(resident=resident)
+
+        total_bills = bills.count()
+
+        return Response({'total_bills': total_bills}, status=status.HTTP_200_OK)
+
+    @action(methods=['post'], detail=False, url_path='create-bill')
+    def create_bill(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return Response({"error": "Only superusers can create bills"}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['delete'], url_path='delete-bill')
+    def delete_bill(self, request, pk=None):
+        try:
+            bill = self.get_object()
+            bill.delete()
+            return Response({"message": "Bill deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
+        except Bill.DoesNotExist:
+            return Response({"error": "Bill not found."}, status=status.HTTP_404_NOT_FOUND)
